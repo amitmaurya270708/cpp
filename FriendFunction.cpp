5ad0671af65346ba293db91acf217fe15abc4e48
Amit <amitkumarmaurya270708@gmail.com>
2026-08-01 18:14:36 +0530

    Day 40

---
+++ FriendFunction.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Ankit;
+class Ankush{
+    private:
+    int money = 10;
+    friend void rohit(Ankit,Ankush);
+};
+class Ankit{
+    int money = 20;
+    friend void rohit(Ankit,Ankush);
+};
+void rohit(Ankit r1, Ankush r2){
+    cout << "sum "<< r1.money + r2.money << endl;
+}
+main(){
+    Ankush obj1;
+    Ankit obj2;
+    rohit(obj2, obj1);
+}
---
+++ FriendFunction.md
@@ -0,0 +1,5 @@
+# Friend Function 
+Friend function is function that is declared as a friend of a class not as a member of a class instead of that it can access private & protected member of class.
+
+## Syntax
+    friend return_type func_name(class & ref);
